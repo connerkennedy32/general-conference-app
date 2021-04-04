be9507357bc66ee1f87876d5951a5aeeb88c022b
Conner <connerkennedy32@gmail.com>
2021-04-04 08:00:54 -0600

    First Commit

---
+++ front-end/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <form action="">
+      <input type="text" v-model="test" placeholder="TEST">
+      <button type="submit">Submit</button>
+    </form>
+    <p>{{test}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      test: "",
+    }
+  },
+}
+</script>
