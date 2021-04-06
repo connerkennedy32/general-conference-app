214097f69407fde5f4049a3aa5c324476cafb408
Conner <connerkennedy32@gmail.com>
2021-04-05 20:28:12 -0600

    Test from VS code

--- back-end/server.js
+++ back-end/server.js
@@ -77,6 +77,6 @@ app.delete('/api/items/:id', async (req, res) => {
     }
 });
 
-  app.listen(3000, () => console.log('Server listening on port 3000!'));
+  app.listen(3001, () => console.log('Server listening on port 3001!'));
 
   
