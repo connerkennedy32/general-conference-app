771ce70c3ddade5120d067e8cde2902f8112794e
Conner <connerkennedy32@gmail.com>
2021-04-06 17:30:43 -0600

    added github link

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -5,10 +5,24 @@
       <router-link to="/admin">Admin</router-link>
     </div>
     <router-view />
+    <div class="footer">
+      <div class="container" style="padding: 10px"><a href="https://github.com/connerkennedy32/general-conference-app">GitHub Link</a></div>
+    </div>
   </div>
+  
 </template>
 
 <style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: rgb(218, 218, 218);
+    color: white;
+    text-align: center;
+}
+
 #app {
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
