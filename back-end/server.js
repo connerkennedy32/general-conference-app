296959756870799b24732ed85b4311643ff6b9e0
Conner <connerkennedy32@gmail.com>
2021-04-19 17:34:42 -0600

    Finished Authentication

--- back-end/server.js
+++ back-end/server.js
@@ -28,73 +28,12 @@ app.use(cookieSession({
   }
 }));
 
-// Create a scheme for talks: a path and a bool to see if I have listened to it yet.
-const itemSchema = new mongoose.Schema({
-    path: String,
-    hasListened: Boolean
-  });
-
-// Create a model for items in the museum.
-const Item = mongoose.model('Item', itemSchema);
-
-app.post('/api/items', async (req, res) => {
-    const item = new Item({
-      path: req.body.path,
-      hasListened: false,
-    });
-    try {
-      await item.save();
-      res.send(item);
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(500);
-    }
-  });
-
-app.get('/api/items', async (req, res) => {
-    try {
-      let items = await Item.find();
-      res.send(items);
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(500);
-    }
-  });
-
-app.put('/api/items/:id', async (req,res) => {
-    try {
-        let temp = await Item.findOne({
-            _id: req.params.id
-        });
-        if (temp.hasListened == false) {
-            temp.hasListened = true;
-        } else {
-            temp.hasListened = false;
-        }
-        temp.save();
-        res.sendStatus(200);
-    } catch(error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-});
-
-app.delete('/api/items/:id', async (req, res) => {
-    try {
-        await Item.deleteOne({
-            _id: req.params.id
-        });
-        res.sendStatus(200);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-});
-
-
 // import the users module and setup its API path
 const users = require("./users.js");
 app.use("/api/users", users.routes);
+
+const items = require("./links.js");
+app.use("/api/items", items.routes);
 
   app.listen(3001, () => console.log('Server listening on port 3001!'));
 
